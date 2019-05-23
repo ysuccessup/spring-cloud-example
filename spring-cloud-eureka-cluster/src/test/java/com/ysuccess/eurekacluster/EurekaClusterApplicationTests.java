49e81eb1165e3f84d0c37109c2096f04de80641f
yangfei <ysuccess_up@163.com>
2019-05-24 00:48:57 +0800

    eureka cluster

--- spring-cloud-eureka-cluster/src/test/java/com/ysuccess/eurekacluster/EurekaClusterApplicationTests.java
+++ spring-cloud-eureka-cluster/src/test/java/com/ysuccess/eurekacluster/EurekaClusterApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ysuccess.eureka;
+package com.ysuccess.eurekacluster;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaApplicationTests {
+public class EurekaClusterApplicationTests {
 
     @Test
     public void contextLoads() {
