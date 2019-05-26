bb641d8f0d24132f4968dff024046235ead9e655
yangfei <ysuccess_up@163.com>
2019-05-26 19:28:03 +0800

    add for zuul retry method

--- spring-cloud-eureka-provider/src/main/java/com/ysuccess/eurekaprovider/controller/HiController.java
+++ spring-cloud-eureka-provider/src/main/java/com/ysuccess/eurekaprovider/controller/HiController.java
@@ -1,5 +1,7 @@
 package com.ysuccess.eurekaprovider.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,11 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("ysuccessup")
 public class HiController {
 
+    private  final Logger logger = LoggerFactory.getLogger(HiController.class);
+
     @Value("${server.port}")
     String port;
 
     @RequestMapping("/hi")
     public String home(@RequestParam(value = "name", defaultValue = "yy") String name) {
         return "hi " + name + " ,i am from port:" + port;
+    }
+
+    @RequestMapping("/hello")
+    public String index(@RequestParam(value = "name", defaultValue = "yy") String name) {
+        logger.info("request two name is "+name);
+        try{
+            Thread.sleep(1000000);
+        }catch ( Exception e){
+            logger.error(" hello two error",e);
+        }
+        return "hello "+name+"，this is two messge";
     }
 }
