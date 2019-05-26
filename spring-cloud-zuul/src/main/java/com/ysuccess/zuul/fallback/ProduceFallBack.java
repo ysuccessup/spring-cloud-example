576cc9f1d0ca78f438f52a0715ebd871173002ab
yangfei <ysuccess_up@163.com>
2019-05-26 17:59:46 +0800

    add zuul fallback

---
+++ spring-cloud-zuul/src/main/java/com/ysuccess/zuul/fallback/ProduceFallBack.java
@@ -0,0 +1,71 @@
+package com.ysuccess.zuul.fallback;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Create by idea
+ *
+ * @Author: ysuccessup
+ * @Date: 2019/5/26 17:10
+ */
+@Component
+public class ProduceFallBack implements FallbackProvider {
+    @Override
+    public String getRoute() {
+        return "service-hi";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        if (cause != null && cause.getCause() != null) {
+            String reason = cause.getCause().getMessage();
+            System.out.println(reason);
+        }
+        return fallbackResponse();
+    }
+
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("The service is unavailable.".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
