7200136fe1a5704af1d42135680696aa41b360a5
yangfei <ysuccess_up@163.com>
2019-05-27 00:23:42 +0800

    add consul producer2 for test loadbalance

---
+++ spring-cloud-consul-producer2/src/main/java/com/ysuccess/consulproducer2/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.ysuccess.consulproducer2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Create by idea
+ *
+ * @Author: ysuccessup
+ * @Date: 2019/5/26 23:39
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello consul two";
+    }
+}
